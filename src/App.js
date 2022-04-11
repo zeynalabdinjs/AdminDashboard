668f949983615e3907f3554ce8f069fcc8729955
Zeynalabdin <zeeynalabdin@gmail.com>
2022-04-12 02:20:50 +0400

    up

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Navbar from './pages/Navbar';
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import { BarLoader } from 'react-spinners';
+import { css } from '@emotion/react';
 
 
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  // useEffect(() => {
+  //   setLoading(true)
+  //   setTimeout(() => {
+  //     setLoading(false)
+  //   }, 2000)
+  // }, [])
+
+
+
+
   const modeState = useSelector(state => state.mode.value)
 
   const active = modeState ? 'active' : '';
@@ -22,21 +34,42 @@ function App() {
       document.body.classList.remove('active');
     }
   });
+  useEffect(() => {
+    window.onload = () => {
+      setLoading(false)
+    }
+  })
+
+
+
   return (
-    <div className={`App ${active}`}>
-      <Dashboard />
-      <Navbar />
+    <>
+      {loading ?
+        <div className='loading d-flex w-100 vh-100 justify-content-center align-items-center'>
+          <BarLoader
+            size={80}
+            color={"#9814eb"}
+            loading={loading}
+          />
+        </div>
+        :
+        <div className={`myApp ${active}`}>
+
+          <Dashboard />
+          <Navbar />
 
 
-      {/* roots */}
-      <div className="main-container">
+          {/* roots */}
+          <div className="main-container">
 
 
-        <Home />
+            <Home />
 
-      </div>
-      {/* roots */}
-    </div>
+          </div>
+          {/* roots */}
+        </div>
+      }
+    </>
   );
 }
 
